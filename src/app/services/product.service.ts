2cb025cd96109a058a0cd4f45425fe523cbb480c
stebinpulikkan <stebinpulikkan@gmail.com>
2019-06-07 21:54:23 +0530

    add name

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -7,9 +7,9 @@ export class productService {
         { img: "https://cdn.pixabay.com/photo/2015/12/15/09/04/banner-1093909__340.jpg" },
          ]
     products =[ 
-        { img: "https://colorway.foundry.com/site/assets/files/1031/slide1-img.png" },
-        { img: "https://consumer-img.huawei.com/content/dam/huawei-cbg-site/common/mkt/pdp/phones/y9-2019/img/huawei-y9-2019-kv-product.png" },
-        { img: "http://www.plantronics.com/content/dam/plantronics/images/catalog-content/voyager-3200-/voyager-3200-black-with-case-product-page.png" },
+        { img: "https://colorway.foundry.com/site/assets/files/1031/slide1-img.png" ,name:"FZ125"},
+        { img: "https://consumer-img.huawei.com/content/dam/huawei-cbg-site/common/mkt/pdp/phones/y9-2019/img/huawei-y9-2019-kv-product.png",name:"Cover135" },
+        { img: "http://www.plantronics.com/content/dam/plantronics/images/catalog-content/voyager-3200-/voyager-3200-black-with-case-product-page.png",name:"Case-care" },
     ]
     
     constructor() { }
